46cd9a83b5f85f0e55dc158c5158ab398009a144
citlalig <citlalig@Citlalis-MacBook-Air.local>
2014-10-17 19:01:08 -0500

    Initial Commit

---
+++ t3_mob_uag/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  t3_mob_uag
+//
+//  Created by Citlali Garcia on 10/17/14.
+//  Copyright (c) 2014 Personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ t3_mob_uagTests/t3_mob_uagTests.m
@@ -0,0 +1,40 @@
+//
+//  t3_mob_uagTests.m
+//  t3_mob_uagTests
+//
+//  Created by Citlali Garcia on 10/17/14.
+//  Copyright (c) 2014 Personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface t3_mob_uagTests : XCTestCase
+
+@end
+
+@implementation t3_mob_uagTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
